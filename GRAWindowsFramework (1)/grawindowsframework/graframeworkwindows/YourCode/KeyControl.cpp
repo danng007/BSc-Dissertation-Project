c431893ba2b0e239158380252d862b66ac379b50
danng007 <wantaotang@gmail.com>
2016-02-02 22:55:52 +0800

    GitHubTest
    Delete some comment

--- GRAWindowsFramework (1)/grawindowsframework/graframeworkwindows/YourCode/KeyControl.cpp
+++ GRAWindowsFramework (1)/grawindowsframework/graframeworkwindows/YourCode/KeyControl.cpp
@@ -11,27 +11,6 @@ KeyControl::~KeyControl()
 
 void KeyControl::HandleKey(int key, int state, int x, int y)
 {
-	// toggle booleans to remember which key was pressed/released ready for the update function
-	/*switch (key) {
-	case 'A':
-	case 'a':
-		aKey = state;
-		break;
-	case 'D':
-	case 'd':
-		dKey = state;
-		break;
-	case 'W':
-	case 'w':
-		wKey = state;
-		break;
-	case 'S':
-	case 's':
-		sKey = state;
-		break;
-	default:
-		break;
-	}*/
 	if (state == 0 && key == 'q')
 	{
 		gameStart = !gameStart;
